01249192402ae81adbdcbbfc814ea52fcfb4d624
AriannaToitonova <ariannatoitonova@gmail.com>
2023-06-02 22:31:11 +0600

    dz6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Slider() {
+    const [slide, setSlide] = useState(0);
+
+    const urls = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx9gwmxRltZvcxZJpTk9I95XNjhzlCCOph6mFbMZNnePwJzhgi47x7eoc6OuIvIKetxfI&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcipQHRmXy9spfiYUoRJ-mKwQJj1EZybQTlLbdmvJiM-8gS-Fq0S52MyNK7mmwKtBLdX8&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAh9FzAEh8sguYc-q07axKfQQqCxIAT8k3ABwRBLRzYmKSOiaeL5kHdX3IIC8QYgg9frw&usqp=CAU",
+    ];
+
+    const nextSlide = () => {
+        setSlide(
+            (prevSlide) =>
+                (prevSlide === urls.length - 1 ? 0 : prevSlide + 1));
+    };
+
+    const prevSlide = () => {
+        setSlide(
+            (prevSlide) =>
+                (prevSlide === 0 ? urls.length - 1 : prevSlide - 1));
+    };
+    return (
+        <div className="slider">
+            <button
+                className="slider-button"
+                onClick={prevSlide}>
+                Назад
+            </button>
+            <img
+                className="slider-image"
+                src={urls[slide]}
+                alt="Фотография"
+            />
+            <button
+                className="slider-button"
+                onClick={nextSlide}>
+                Далее
+            </button>
+        </div>
+    );
 }
-
-export default App;
